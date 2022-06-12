fd592e39bfb7ae6a8ba2e528c63e3174f031b6e5
José de Jesus Filho <jjesusfilho@gmail.com>
2022-06-11 22:47:00 -0300

    atualiza bases

--- R/ler_cjpg.R
+++ R/ler_cjpg.R
@@ -5,11 +5,10 @@ cjpg <- tjsp_ler_cjpg(diretorio = "data-raw/cjpg")
 saveRDS(cjpg, "data/cjpg.rds")
 save(cjpg, file = "data/cjpg.rda")
 
-cjpg1 <- cjpg[1:6000,]
 saveRDS(cjpg1, "data/cjpg1.rds")
 
 lista <- cjpg |> 
-       mutate(grupo = ntile(n=10)) |> 
+       #mutate(grupo = ntile(n=10)) |> 
        group_split(grupo)
 
 nomes <- paste0("data/cjpg", 1:10,".rds")
@@ -25,4 +24,15 @@ arquivos <- list.files("data", full.names = T)
 
 cjpg <- map_dfr(arquivos, ~readRDS(.x))
 
-# comentário final
+cjpg <- cjpg |> 
+      mutate(decisao = tjsp_classificar_sentenca(julgado))
+
+
+cjpg <- cjpg |> 
+      filter(str_detect(decisao, "(parcial|procedente)"))
+
+
+valor <- str_extract_all(cjpg$julgado[1:1000], "R\\$\\s?\\d\\S+")
+
+
+cjpg$julgado[976]
